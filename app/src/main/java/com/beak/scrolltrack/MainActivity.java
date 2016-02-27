6dc29b3cc692d7791db8f91aaa020f12329e8197
mirsfang <mirsfang@163.com>
2016-02-27 09:50:16 +0800

    Initial commit

---
+++ app/src/main/java/com/beak/scrolltrack/MainActivity.java
@@ -0,0 +1,90 @@
+package com.beak.scrolltrack;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.beak.scrolltrack.scroll.BottomTrackListener;
+import com.beak.scrolltrack.scroll.TopDecoration;
+import com.beak.scrolltrack.scroll.TopTrackListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mMainRecyclerView = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMainRecyclerView = (RecyclerView)findViewById(R.id.main_recycler_view);
+        mMainRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mMainRecyclerView.addItemDecoration(new TopDecoration(findViewById(R.id.main_top_view)));
+        mMainRecyclerView.addOnScrollListener(new TopTrackListener(findViewById(R.id.main_top_view)));
+        mMainRecyclerView.addOnScrollListener(new BottomTrackListener(findViewById(R.id.main_bottom_view)));
+
+        mMainRecyclerView.setAdapter(new SimpleAdapter());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main_check_resource_code:
+                try {
+                    Intent it = new Intent(Intent.ACTION_VIEW);
+                    it.setData(Uri.parse("https://github.com/boybeak/ScrollTrack"));
+                    startActivity(it);
+                } catch (Exception e) {
+                    Toast.makeText(this, "no browser found for view code web page", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class SimpleAdapter extends RecyclerView.Adapter<SimpleHolder> {
+
+        @Override
+        public SimpleHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new SimpleHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.market_rv_item, null));
+        }
+
+        @Override
+        public void onBindViewHolder(SimpleHolder holder, int position) {
+            holder.mTv.setText(position + "");
+        }
+
+        @Override
+        public int getItemCount() {
+            return 200;
+        }
+    }
+
+    private class SimpleHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTv = null;
+
+        public SimpleHolder(View itemView) {
+            super(itemView);
+            mTv = (TextView)itemView.findViewById(R.id.item_tv);
+        }
+    }
+}
---
+++ app/src/main/java/com/beak/scrolltrack/scroll/TopDecoration.java
@@ -0,0 +1,25 @@
+package com.beak.scrolltrack.scroll;
+
+import android.graphics.Rect;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by gaoyunfei on 15/10/11.
+ */
+public class TopDecoration extends RecyclerView.ItemDecoration {
+
+    private View mTopView = null;
+
+    public TopDecoration (View topView) {
+        mTopView = topView;
+    }
+
+    @Override
+    public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+        super.getItemOffsets(outRect, view, parent, state);
+        if (parent.getChildAdapterPosition(view) == 0) {
+            outRect.set(0, mTopView.getHeight(), 0, 0);
+        }
+    }
+}
